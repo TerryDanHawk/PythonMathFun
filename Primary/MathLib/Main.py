c0d1279a672022b715f9e5388b772529dd6b625e
TerryDan <dantao332@msn.cn>
2019-03-12 00:30:23 +0800

    saved

--- Primary/MathLib/Main.py
+++ Primary/MathLib/Main.py
@@ -6,7 +6,8 @@ from Probability.Functions import *
 
 
 def main():
-    #print(str(A(4,2)) + ":" + str(C(4,2)))
+    # print(str(A(4,2)) + ":" + str(C(4,2)))
+    print("Hello")
 
 if __name__ == '__main__':
         main()
