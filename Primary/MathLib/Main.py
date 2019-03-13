db4202b6debbbb8a9937ae947d33f8d9cfc83469
TerryDan <dantao332@msn.cn>
2019-03-13 23:41:15 +0800

    add function of ExpandBinomial

--- Primary/MathLib/Main.py
+++ Primary/MathLib/Main.py
@@ -6,8 +6,13 @@ from Probability.Functions import *
 
 
 def main():
-    # print(str(A(4,2)) + ":" + str(C(4,2)))
-    print("Hello")
+    #排列与组合，从4个中选取2个
+    #print(str(A(4,2)) + ":" + str(C(4,2)))
+
+    #牛顿二项式定理
+    #print(ExpandBinomial(4))
+
+
 
 if __name__ == '__main__':
         main()
--- Primary/MathLib/Probability/Functions.py
+++ Primary/MathLib/Probability/Functions.py
@@ -34,3 +34,42 @@ def C(n,m):
 
     result=F(n)//(F(m)*F(n-m)) if (F(m)*F(n-m)) != 0 else 0
     return  result
+
+
+'''
+用牛顿定理展开二项式
+(x+y)^n
+其中n为幂
+'''
+def  ExpandBinomial(n):
+    result=""
+    for k in range(0,n+1,1):
+        item=""
+        a1=C(n,k)
+        a2=n-k
+        a3=k
+        if a1!=1:
+            item += str(a1)
+
+        if a2==0:
+            item += ""
+        elif a2==1:
+            item += "x"
+        else:
+            item += "x^" + str(a2)+" "
+
+        if a3==0:
+            item += ""
+        elif a3==1:
+            item += "y"
+        else:
+            item += "y^" + str(a3)+" "
+
+
+        if item=="":
+            item="1"
+        result+=item+"+"
+    if len(result) > 0:
+        result = result[0:len(result) - 1]
+
+    return  result
